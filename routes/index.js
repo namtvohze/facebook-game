4fe6d6891e992e119f0930077bff3faa1594dfa4
Trinh Viet Nam <trinhvietnambk@gmail.com>
2017-06-27 17:09:12 +0700

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var ejs = require('ejs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.post('/', function(req, res, next) {
+    ejs.renderFile(__dirname+'/../views/index.ejs', { title: 'Express' }, {}, function(err, str){
+        res.end(str);
+    });
 });
 
 module.exports = router;
